8cb313bf7caa92cd6e6e3fbab798e765f9161c2a
Shivani <kkrishnashivani18@gmail.com>
2025-01-24 20:13:49 +0530

    updated some frontend files

--- frontend/react/src/components/Transactions.jsx
+++ frontend/react/src/components/Transactions.jsx
@@ -1,37 +1,3 @@
-/*
-import React, { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
-
-export function Transactions() {
-    const { transactions, deleteTransaction } = useContext(GlobalContext); 
-
-    return (
-        <div>
-            <h3 className='font-semibold'> Previous Transactions </h3>
-            <ul className="list">
-                {transactions.map((transaction) => (
-                    <li     
-                        key={transaction.id}
-                        className={transaction.amount > 0 ? "plus" : "minus"}
-                        >
-                        {transaction.text}{" "} 
-                        <span>
-                            {transaction.amount > 0 ? "+ ₹" : "- ₹"}
-                            {Math.abs(transaction.amount)}
-                        </span>
-                        <button
-                            className='delete-btn'
-                            onClick={() => deleteTransaction(transaction.id)}> x </button>    
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
-*/
-
-//--------------------------------------------------------------------------------------------------------------------------------------------------
-
 import React, { useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
